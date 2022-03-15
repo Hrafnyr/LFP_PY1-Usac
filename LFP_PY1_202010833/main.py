23789305ff2516f82ff5fbf3da23054a9030954b
Hrafnyr <moiseszurdomaldonado@gmail.com>
2022-03-15 01:24:48 -0600

    Botón de cargar archivo funcional

--- LFP_PY1_202010833/main.py
+++ LFP_PY1_202010833/main.py
@@ -1,9 +1,38 @@
+
 import tkinter as tk
 from tkinter import *
 from tkinter import ttk
 from tkinter import scrolledtext as stxt
+from tkinter import filedialog
+from tkinter import messagebox as MessageBox
 
 
+
+def abrirDocumentoForm():
+    Tk().withdraw() #Remover ventana
+    archivo1 = filedialog.askopenfile(       #Abrir ventana
+        title = "Seleccione un archivo",   #Información solicitada
+        initialdir = "./",
+        filetypes = [
+            ("Archivos .form", "*.form"),
+            ("Archivos .lfp", "*.lfp"),
+            ("Todos los archivos",  "*.*")
+        ]
+    )
+
+    if archivo1 is None:  #Verifica que si existe selección de archivos
+        MessageBox.showinfo('Atención','No se seleccionó ningún archivo')
+        return None
+    else:
+        ruta = archivo1.name
+        t = open(ruta, 'r',encoding='utf-8')  #Si se seleccionó, leer el archivo y cerrarlo
+        texto = t.read()
+        mostrarDatos(texto)
+        t.close()
+        return texto
+
+def mostrarDatos(texto):
+    txt.insert(INSERT,texto)
 
 #Interfaz gráfica
 root = tk.Tk()                 #Raiz           
@@ -15,7 +44,7 @@ root.config(bg="#87F7EC")
 root.eval('tk::PlaceWindow . center')
 
 #Botón 1
-botonCargar = tk.Button(text="Cargar archivo")
+botonCargar = tk.Button(text="Cargar archivo", command=abrirDocumentoForm)
 botonCargar.place(x=25, y=20)
 botonCargar.config(font=("Courier", 12), bg="#0A1246",fg="white",width=15)
 
